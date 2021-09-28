b92dd407938554332affff853e352c85e446e3cf
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2021-09-28 11:23:41 +0200

    fixed tests

--- tests/test_tokamak_source.py
+++ tests/test_tokamak_source.py
@@ -1,9 +1,26 @@
-from openmc_plasma_source import TokamkaSource
+from openmc_plasma_source import TokamakSource
 
 from openmc import Source
 
 
 def test_creation():
-    my_source = TokamkaSource()
+    my_source = TokamakSource(
+        elongation=1.557,
+        ion_density_centre=1.09e20,
+        ion_density_peaking_factor=1,
+        ion_density_pedestal=1.09e20,
+        ion_density_separatrix=3e19,
+        ion_temperature_centre=45.9,
+        ion_temperature_peaking_factor=8.06,
+        ion_temperature_pedestal=6.09,
+        ion_temperature_separatrix=0.1,
+        major_radius=9.06,
+        minor_radius=2.92258,
+        pedestal_radius=0.8 * 2.92258,
+        mode="H",
+        shafranov_factor=0.44789,
+        triangularity=0.270,
+        ion_temperature_beta=6
+    )
     for source in my_source:
         assert isinstance(source, Source)
