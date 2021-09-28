9d13a4a1dc945c01717c6f83a549c9c510a15599
AI-Pranto <pranto.roza.53@gmail.com>
2021-09-28 21:28:13 +0600

    update test

--- tests/test_point_source.py
+++ tests/test_point_source.py
@@ -1,8 +1,11 @@
 from openmc_plasma_source import FusionPointSource
 
 from openmc import Source
-
+import pytest
 
 def test_creation():
     my_source = FusionPointSource()
     assert isinstance(my_source, Source)
+
+with pytest.raises(ValueError):
+    FusionRingSource(radius=1, fuel='топливо')
