1b98b522d1c489b40ba206c81cf637f5efc3bb69
Liam Pattinson <liampattinson@gmail.com>
2022-02-11 15:41:57 +0000

    Upgraded TokamakSource plotting functions and expanded tests
    - Can now pass Axes into plotting functions, can be used when the
      user wishes to make use of subplots etc.
    - Operations converted to make better use of NumPy
    - Better error reporting from plotting functions
    - Plotting tests greatly expanded, now produces example plots

--- openmc_plasma_source/plotting/plot_tokamak_source.py
+++ openmc_plasma_source/plotting/plot_tokamak_source.py
@@ -3,81 +3,151 @@ from matplotlib import cm
 import numpy as np
 
 
-def scatter_tokamak_source(source, quantity=None, **kwargs):
+def scatter_tokamak_source(source, ax=None, quantity=None, aspect="equal", **kwargs):
     """Create a 2D scatter plot of the tokamak source.
     See https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.scatter.html
     for more arguments.
 
     Args:
         source (ops.TokamakSource): the plasma source
-        quantity ("str", optional): value by which the lines should be
+        ax (maplotlib.Axes, optional): Axes object on which to plot. If not
+            provided by the user, the current working Axes is retrieved using
+            matplotlib.pyplot.gca().
+        aspect (str, optional): Set aspect ratio of the plot. May be set to 'auto',
+            'equal', or a number denoting the ratio of the plot height to the plot
+            width.
+        quantity (str, optional): value by which the lines should be
             coloured. Defaults to None.
+        **kwargs: Keyword arguments compatible with matplotlib.pyplot.scatter
 
     Raises:
         ValueError: If the quantity is unknown
     """
 
-    quantity_to_attribute = {
-        "ion_temperature": source.temperatures,
-        "neutron_source_density": source.neutron_source_density,
-        "strength": source.strengths,
-    }
-    if quantity in quantity_to_attribute:
-        colours = quantity_to_attribute[quantity]
-    elif quantity is None:
-        colours = None
-    else:
-        raise ValueError("Unknown quantity")
-    plt.gca().set_aspect("equal")
+    # Define possible quantities, and link to arrays within tokamak_source
+    # If given a non-TokamakSource, this step will fail with an AttributeError
+    try:
+        quantity_to_attribute = {
+            "ion_temperature": source.temperatures,
+            "neutron_source_density": source.neutron_source_density,
+            "strength": source.strengths,
+        }
+    except AttributeError as e:
+        raise ValueError(
+            f"openmc_plasma_source.scatter_tokamak_source: argument 'source' "
+            f"must be of type TokamakSource"
+        ) from e
 
-    return plt.scatter(source.RZ[0], source.RZ[1], c=colours, **kwargs)
+    # For a given quantity, determine colours to plot for each point
+    # If given an incorrect quantity name, this step will fail with a KeyError
+    colours = None
+    if quantity is not None:
+        try:
+            colours = quantity_to_attribute[quantity]
+        except KeyError as e:
+            raise ValueError(
+                f"openmc_plasma_source.scatter_tokamak_source: Unknown 'quantity' "
+                f"provided, options are {quantity_to_attribute.keys()}"
+            ) from e
+
+    # If not provided with an Axes instance, retrieve the current Axes in focus
+    if ax is None:
+        ax = plt.gca()
+
+    # Scatter the source R and Z positions, optionally colouring using the chosen
+    # quantity.
+    ax.scatter(source.RZ[0], source.RZ[1], c=colours, **kwargs)
+
+    # Set the aspect ratio on the axes.
+    # Defaults to 'equal', so 1m on the x-axis has the same width as 1m on the y-axis
+    ax.set_aspect(aspect)
+
+    return ax
 
 
 def plot_tokamak_source_3D(
-    source, quantity=None, angles=[0, 1 / 2 * np.pi], colorbar="viridis", **kwargs
+    tokamak_source,
+    ax=None,
+    quantity=None,
+    angles=[0, 0.5 * np.pi],
+    colorbar=None,
+    **kwargs,
 ):
     """Creates a 3D plot of the tokamak source.
     See https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html#matplotlib.pyplot.plot
     for more arguments.
 
     Args:
-        source (ops.TokamakSource): the plasma source
+        tokamak_source (ops.TokamakSource): the plasma source
+        ax (maplotlib.Axes, optional): Axes object on which to plot. If not
+            provided by the user, a new Axes is created by calling
+            matplotlib.pyplot.axes(projection="3d").
         quantity ("str", optional): value by which the lines should be
             coloured. Defaults to None.
         angles (list, optional): iterable of two floats defining the coverage.
             Defaults to [0, 1/2*np.pi].
         colorbar (str, optional): colorbar used if quantity is not None.
-            Defaults to "viridis".
+            When None, matplotlib currently defaults to "viridis".
 
     Raises:
         ValueError: If the quantity is unknown
     """
 
-    quantity_to_attribute = {
-        "ion_temperature": source.temperatures,
-        "neutron_source_density": source.neutron_source_density,
-        "strength": source.strengths,
-    }
-    if quantity in quantity_to_attribute:
-        values = quantity_to_attribute[quantity]
-    elif quantity is None:
-        values = None
+    # Define possible quantities, and link to arrays within tokamak_source
+    # If given a non-TokamakSource, this step will fail with an AttributeError
+    try:
+        quantity_to_attribute = {
+            "ion_temperature": tokamak_source.temperatures,
+            "neutron_source_density": tokamak_source.neutron_source_density,
+            "strength": tokamak_source.strengths,
+        }
+    except AttributeError as e:
+        raise ValueError(
+            f"openmc_plasma_source.scatter_tokamak_source: argument 'source' "
+            f"must be of type TokamakSource"
+        ) from e
+
+    # For a given quantity, get the corresponding array from tokamak_source
+    # If given an incorrect quantity name, this step will fail with a KeyError
+    if quantity is not None:
+        try:
+            quantity_values = quantity_to_attribute[quantity]
+        except KeyError as e:
+            raise ValueError(
+                f"openmc_plasma_source.scatter_tokamak_source: Unknown 'quantity' "
+                f"provided, options are {quantity_to_attribute.keys()}"
+            ) from e
     else:
-        raise ValueError("Unknown quantity")
+        quantity_values = np.ones(tokamak_source.sample_size)
 
-    colorbar = cm.get_cmap(colorbar)
-    axes = plt.axes(projection="3d")
-    theta = np.linspace(*angles, 100)
-    for i in range(source.sample_size):
-        if values is not None:
-            colour = colorbar(values[i] / max(values))
-        else:
-            colour = None
-        x = source.RZ[0][i] * np.sin(theta)
-        y = source.RZ[0][i] * np.cos(theta)
-        z = source.RZ[1][i]
-        plt.plot(x, y, z, color=colour, **kwargs)
+    # Get the colours used to plot each curve
+    # If 'quantity' == None, all have the same colour, selected from the middle
+    # of the colormap.
+    cmap = cm.get_cmap(colorbar)
+    if quantity is not None:
+        colors = cmap(quantity_values / np.max(quantity_values))
+    else:
+        colors = cmap(np.full(tokamak_source.sample_size, 0.5))
 
-    axes.set_xlim(-source.major_radius, source.major_radius)
-    axes.set_ylim(-source.major_radius, source.major_radius)
-    axes.set_zlim(-source.major_radius, source.major_radius)
+    # If not provided with an Axes object, create a new one
+    if ax is None:
+        ax = plt.axes(projection="3d")
+
+    # Get curves to plot
+    n_theta = 100
+    theta = np.linspace(*angles, n_theta)
+    xs = np.outer(tokamak_source.RZ[0], np.sin(theta))
+    ys = np.outer(tokamak_source.RZ[0], np.cos(theta))
+    zs = tokamak_source.RZ[1]
+
+    # Plot each curve in turn
+    for x, y, z, color in zip(xs, ys, zs, colors):
+        ax.plot(x, y, z, color=color, **kwargs)
+
+    # Set plot bounds
+    major_radius = tokamak_source.major_radius
+    ax.set_xlim(-major_radius, major_radius)
+    ax.set_ylim(-major_radius, major_radius)
+    ax.set_zlim(-major_radius, major_radius)
+
+    return ax
--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,50 +1,263 @@
 import matplotlib.pyplot as plt
-from openmc_plasma_source import plotting, TokamakSource
+import numpy as np
+from openmc_plasma_source import (
+    TokamakSource,
+    plotting as ops_plt,
+)
 import pytest
 
-my_plasma = TokamakSource(
-    elongation=1.557,
-    ion_density_centre=1.09e20,
-    ion_density_peaking_factor=1,
-    ion_density_pedestal=1.09e20,
-    ion_density_separatrix=3e19,
-    ion_temperature_centre=45.9,
-    ion_temperature_peaking_factor=8.06,
-    ion_temperature_pedestal=6.09,
-    ion_temperature_separatrix=0.1,
-    major_radius=9.06,
-    minor_radius=2.92258,
-    pedestal_radius=0.8 * 2.92258,
-    mode="H",
-    shafranov_factor=0.44789,
-    triangularity=0.270,
-    ion_temperature_beta=6,
+
+@pytest.fixture
+def tokamak_source():
+    return TokamakSource(
+        elongation=1.557,
+        ion_density_centre=1.09e20,
+        ion_density_peaking_factor=1,
+        ion_density_pedestal=1.09e20,
+        ion_density_separatrix=3e19,
+        ion_temperature_centre=45.9,
+        ion_temperature_peaking_factor=8.06,
+        ion_temperature_pedestal=6.09,
+        ion_temperature_separatrix=0.1,
+        major_radius=9.06,
+        minor_radius=2.92258,
+        pedestal_radius=0.8 * 2.92258,
+        mode="H",
+        shafranov_factor=0.44789,
+        triangularity=0.270,
+        ion_temperature_beta=6,
+        sample_size=500,
+    )
+
+
+def test_scatter_tokamak_source_defaults(tokamak_source):
+    """Ensure plotting is successful without providing additional args"""
+    plt.figure()
+    assert not plt.gca().collections  # Check current ax is empty
+    ops_plt.scatter_tokamak_source(tokamak_source)
+    assert plt.gca().collections  # Check current ax is not empty
+    # Save for viewing, clean up
+    plt.xlabel("R")
+    plt.ylabel("Z", rotation=0)
+    plt.savefig("tests/test_scatter_tokamak_source_defaults.png")
+    plt.close()
+
+
+def test_scatter_tokamak_source_with_ax(tokamak_source):
+    """Ensure plotting is successful for user-provided ax"""
+    fig = plt.figure()
+    ax = fig.gca()
+    assert not ax.collections  # Check ax is empty
+    ops_plt.scatter_tokamak_source(tokamak_source, ax=ax)
+    assert ax.collections  # Check ax is not empty
+    # Save for viewing, clean up
+    ax.set_xlabel("R")
+    ax.set_ylabel("Z", rotation=0)
+    fig.savefig("tests/test_scatter_tokamak_source_with_ax.png")
+    plt.close(fig)
+
+
+def test_scatter_tokamak_source_with_subplots(tokamak_source):
+    """Ensure plotting is successful for multiple user-provided ax"""
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    # Plot on the first axes
+    assert not ax1.collections  # Check ax is empty
+    ops_plt.scatter_tokamak_source(tokamak_source, ax=ax1)
+    assert ax1.collections  # Check ax is not empty
+    # Generate new data
+    tokamak_source.sample_sources()
+    tokamak_source.sources = tokamak_source.make_openmc_sources()
+    # Plot on the other axes
+    assert not ax2.collections  # Check ax is empty
+    ops_plt.scatter_tokamak_source(tokamak_source, ax=ax2)
+    assert ax2.collections  # Check ax is not empty
+    # Save for viewing, clean up
+    ax1.set_xlabel("R")
+    ax1.set_ylabel("Z", rotation=0)
+    ax2.set_xlabel("R")
+    fig.savefig("tests/test_scatter_tokamak_source_subplots.png")
+    plt.close(fig)
+
+
+@pytest.mark.parametrize(
+    "quantity", ["ion_temperature", "neutron_source_density", "strength"]
 )
-my_plasma.sample_sources()
+def test_scatter_tokamak_source_quantities(tokamak_source, quantity):
+    """Plot with colours set by 'quantity'"""
+    fig = plt.figure()
+    ax = fig.gca()
+    assert not ax.collections  # Check ax is empty
+    ops_plt.scatter_tokamak_source(tokamak_source, ax=ax, quantity=quantity)
+    assert ax.collections  # Check ax is not empty
+    # Save for viewing, clean up
+    ax.set_xlabel("R")
+    ax.set_ylabel("Z", rotation=0)
+    fig.savefig(f"tests/test_scatter_tokamak_source_quantities_{quantity}.png")
+    plt.close(fig)
 
 
-def test_scatter_tokamak_source():
+@pytest.mark.parametrize("aspect", ["equal", "auto", 2])
+def test_scatter_tokamak_source_aspect(tokamak_source, aspect):
+    """Plot with various aspect ratios"""
+    fig = plt.figure()
+    ax = fig.gca()
+    assert not ax.collections  # Check ax is empty
+    ops_plt.scatter_tokamak_source(tokamak_source, ax=ax, aspect=aspect)
+    assert ax.collections  # Check ax is not empty
+    # Save for viewing, clean up
+    ax.set_xlabel("R")
+    ax.set_ylabel("Z", rotation=0)
+    fig.savefig(f"tests/test_scatter_tokamak_source_aspect_{aspect}.png")
+    plt.close(fig)
+
+
+@pytest.mark.parametrize("kwargs", [{"alpha": 0.2}, {"marker": "x"}])
+def test_scatter_tokamak_source_kwargs(tokamak_source, kwargs):
+    """Plot with a kwarg compatible with 'scatter'"""
+    fig = plt.figure()
+    ax = fig.gca()
+    assert not ax.collections  # Check ax is empty
+    ops_plt.scatter_tokamak_source(tokamak_source, ax=ax, **kwargs)
+    assert ax.collections  # Check ax is not empty
+    # Save for viewing, clean up
+    ax.set_xlabel("R")
+    ax.set_ylabel("Z", rotation=0)
+    fig.savefig(
+        f"tests/test_scatter_tokamak_source_kwargs_{list(kwargs.keys())[0]}.png"
+    )
+    plt.close(fig)
+
+
+def test_scatter_tokamak_not_source():
+    """Ensure failure when given non-TokamakSource to plot"""
+    with pytest.raises(ValueError) as excinfo:
+        fig = plt.figure()
+        ax = fig.gca()
+        ops_plt.scatter_tokamak_source("hello world", ax=ax)
+        plt.close(fig)
+    assert "TokamakSource" in str(excinfo.value)
+
+
+@pytest.mark.parametrize("quantity", ["coucou", "ion_density", 17])
+def test_scatter_tokamak_wrong_quantity(tokamak_source, quantity):
+    """Ensure failure when incorrect quantity specified"""
+    with pytest.raises(ValueError) as excinfo:
+        fig = plt.figure()
+        ax = fig.gca()
+        ops_plt.scatter_tokamak_source(tokamak_source, ax=ax, quantity=quantity)
+        plt.close(fig)
+    assert "quantity" in str(excinfo.value)
+
+
+def test_plot_tokamak_source_3D_default(tokamak_source):
+    """Ensure plots correctly with default inputs"""
     plt.figure()
-    plotting.scatter_tokamak_source(my_plasma)
-    plotting.scatter_tokamak_source(my_plasma, quantity="ion_temperature")
-    plotting.scatter_tokamak_source(my_plasma, alpha=0.2)
+    assert not plt.gca().lines  # Check current ax is empty
+    ops_plt.plot_tokamak_source_3D(tokamak_source)
+    assert plt.gca().lines  # Check current ax is not empty
+    # Save for viewing, clean up
+    plt.savefig("tests/test_plot_tokamak_source_3D_defaults.png")
+    plt.close()
 
 
-def test_scatter_tokamak_wrong_quantity():
-    plt.figure()
-    with pytest.raises(ValueError):
-        plotting.scatter_tokamak_source(my_plasma, quantity="coucou")
+def test_plot_tokamak_source_3D_with_ax(tokamak_source):
+    """Ensure plots correctly given ax instance"""
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+    assert not ax.lines  # Check ax is empty
+    ops_plt.plot_tokamak_source_3D(tokamak_source, ax=ax)
+    assert ax.lines  # Check ax is not empty
+    # Save for viewing, clean up
+    fig.savefig("tests/test_plot_tokamak_source_3D_with_ax.png")
+    plt.close(fig)
 
 
-def test_plot_tokamak_source():
-    plt.figure()
-    plotting.plot_tokamak_source_3D(my_plasma)
-    plotting.plot_tokamak_source_3D(my_plasma, quantity="ion_temperature")
-    plotting.plot_tokamak_source_3D(my_plasma, alpha=0.2)
-    plotting.plot_tokamak_source_3D(my_plasma, angles=[0, 3.14])
+@pytest.mark.parametrize(
+    "quantity", ["ion_temperature", "neutron_source_density", "strength"]
+)
+def test_plot_tokamak_source_3D_quantities(tokamak_source, quantity):
+    """Ensure plots correctly for each quantity"""
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+    assert not ax.lines  # Check ax is empty
+    ops_plt.plot_tokamak_source_3D(tokamak_source, ax=ax, quantity=quantity)
+    assert ax.lines  # Check ax is not empty
+    # Save for viewing, clean up
+    fig.savefig(f"tests/test_plot_tokamak_source_3D_quantities_{quantity}.png")
+    plt.close(fig)
 
 
-def test_plot_tokamak_wrong_quantity():
-    plt.figure()
-    with pytest.raises(ValueError):
-        plotting.plot_tokamak_source_3D(my_plasma, quantity="coucou")
+@pytest.mark.parametrize("colorbar", ["plasma", "rainbow"])
+def test_plot_tokamak_source_3D_colorbars(tokamak_source, colorbar):
+    """Ensure plots correctly given colorbar choice"""
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+    assert not ax.lines  # Check ax is empty
+    ops_plt.plot_tokamak_source_3D(
+        tokamak_source, ax=ax, quantity="ion_temperature", colorbar=colorbar
+    )
+    assert ax.lines  # Check ax is not empty
+    # Save for viewing, clean up
+    fig.savefig(f"tests/test_plot_tokamak_source_3D_colorbar_{colorbar}.png")
+    plt.close(fig)
+
+
+@pytest.mark.parametrize(
+    "angles,name",
+    [
+        [(0, np.pi / 4), "eighth"],
+        [(0, np.pi), "half"],
+        [(np.pi, 2 * np.pi), "half_offset"],
+        [(0, 2 * np.pi), "full"],
+    ],
+)
+def test_plot_tokamak_source_3D_angles(tokamak_source, angles, name):
+    """Ensure plots correctly given angles range"""
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+    assert not ax.lines  # Check ax is empty
+    ops_plt.plot_tokamak_source_3D(
+        tokamak_source, ax=ax, quantity="ion_temperature", angles=angles
+    )
+    assert ax.lines  # Check ax is not empty
+    # Save for viewing, clean up
+    fig.savefig(f"tests/test_plot_tokamak_source_3D_angles_{name}.png")
+    plt.close(fig)
+
+
+@pytest.mark.parametrize("kwargs", [{"alpha": 0.2}, {"linestyle": "dotted"}])
+def test_plot_tokamak_source_3D_kwargs(tokamak_source, kwargs):
+    """Ensure plots correctly given additonal keyword arguments to pass on to plot"""
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+    assert not ax.lines  # Check ax is empty
+    ops_plt.plot_tokamak_source_3D(
+        tokamak_source, ax=ax, quantity="ion_temperature", **kwargs
+    )
+    assert ax.lines  # Check ax is not empty
+    # Save for viewing, clean up
+    fig.savefig(
+        f"tests/test_plot_tokamak_source_3D_kwargs_{list(kwargs.keys())[0]}.png"
+    )
+    plt.close(fig)
+
+
+def test_plot_tokamak_source_3D_not_source():
+    """Ensure failure when given non-TokamakSource to plot"""
+    with pytest.raises(ValueError) as excinfo:
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1, projection="3d")
+        ops_plt.plot_tokamak_source_3D("hello world", ax=ax)
+        plt.close(fig)
+    assert "TokamakSource" in str(excinfo.value)
+
+
+@pytest.mark.parametrize("quantity", ["coucou", "ion_density", 17])
+def test_scatter_tokamak_wrong_quantity(tokamak_source, quantity):
+    """Ensure failure when incorrect quantity specified"""
+    with pytest.raises(ValueError) as excinfo:
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1, projection="3d")
+        ops_plt.plot_tokamak_source_3D(tokamak_source, ax=ax, quantity=quantity)
+        plt.close(fig)
+    assert "quantity" in str(excinfo.value)
