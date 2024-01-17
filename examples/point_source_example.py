7e7bba829984465b32766d5d97a7f8644d3efacc
Jonathan Shimwell <mail@jshimwell.com>
2024-01-17 15:27:58 +0000

    simpler examples

--- examples/point_source_example.py
+++ examples/point_source_example.py
@@ -1,23 +1,17 @@
 import openmc
 from openmc_plasma_source import FusionPointSource
 
-my_source = FusionPointSource(coordinate=(0, 0, 0), temperature=20000.0, fuel="DT")
+# minimal geometry
+sphere_surface = openmc.Sphere(r=1000.0, boundary_type='vacuum')
+cell = openmc.Cell(region=-sphere_surface)
+geometry = openmc.Geometry([cell])
 
-# Create a single material
-iron = openmc.Material()
-iron.set_density("g/cm3", 5.0)
-iron.add_element("Fe", 1.0)
-mats = openmc.Materials([iron])
-
-# Create a 5 cm x 5 cm box filled with iron
-cells = []
-inner_box1 = openmc.ZCylinder(r=600.0)
-inner_box2 = openmc.ZCylinder(r=1400.0)
-outer_box = openmc.model.rectangular_prism(4000.0, 4000.0, boundary_type="vacuum")
-cells += [openmc.Cell(fill=iron, region=-inner_box1)]
-cells += [openmc.Cell(fill=None, region=+inner_box1 & -inner_box2)]
-cells += [openmc.Cell(fill=iron, region=+inner_box2 & outer_box)]
-geometry = openmc.Geometry(cells)
+# define the source
+my_source = FusionPointSource(
+    coordinate=(0, 0, 0),
+    temperature=20000.0,
+    fuel="DT"
+)
 
 # Tell OpenMC we're going to use our custom source
 settings = openmc.Settings()
@@ -26,19 +20,9 @@ settings.batches = 10
 settings.particles = 1000
 settings.source = my_source
 
-# Finally, define a mesh tally so that we can see the resulting flux
-mesh = openmc.RegularMesh()
-mesh.lower_left = (-2000.0, -2000.0)
-mesh.upper_right = (2000.0, 2000.0)
-mesh.dimension = (50, 50)
-
-tally = openmc.Tally()
-tally.filters = [openmc.MeshFilter(mesh)]
-tally.scores = ["flux"]
-tallies = openmc.Tallies([tally])
 
 model = openmc.model.Model(
-    materials=mats, geometry=geometry, settings=settings, tallies=tallies
+    materials=None, geometry=geometry, settings=settings
 )
 
 model.run()
